9ff7070eb9548866afab34d3503e5add2152444d
Precious Usanga <precioususanga001@gmail.com>
2020-10-25 08:05:15 +0100

    added server.js fo deployment to heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+//Install express server
+const express = require('express');
+// const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/custom-zoom-webapp'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/custom-zoom-webapp/'}
+);
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 4200);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  signatureEndpoint: 'https://custom-zoom-webapp.herokuapp.com/',
+  apiKey: '9AsIgkT3TIekJfpRAnsP_g',
+  leaveUrl: 'http://localhost:4200/meeting',
+
 };
